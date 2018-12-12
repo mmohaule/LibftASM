cf79e74165f80b52708140a6aa97137a92ba1b92
Mthokozisi MOHAULE <mmohaule@e4r13p10.wethinkcode.co.za>
2018-12-12 15:55:32 +0200

    read multiple times

--- ft_cat.asm
+++ ft_cat.asm
@@ -1,23 +1,33 @@
 segment .bss
-buff resb  100
+buff resb  10
+
+
+segment .data
+data    db  "data", 0x0a
 
 segment		.text
 global		_ft_cat
 
 _ft_cat:
+    mov rbx, rdi
+
+read:
+    mov rdi, rbx
     mov rax, 0x2000003
     mov rsi, buff
-    mov rdx, 100
+    mov rdx, 5
     syscall
 
     cmp rax, 0
     je exit
-    
+
     mov rax, 0x2000004
     mov rdi, 1
     mov rsi, buff
     mov rdx, 100
     syscall
     
-    exit:
-        ret
+    jmp read
+
+exit:
+    ret
