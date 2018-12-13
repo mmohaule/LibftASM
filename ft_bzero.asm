dfd3e4c1680fa258e8d0a1cb65cadc36edf9492f
Mthokozisi MOHAULE <mmohaule@e4r13p10.wethinkcode.co.za>
2018-12-13 09:01:46 +0200

    set gitignore

---
+++ ft_bzero.asm
@@ -0,0 +1,12 @@
+segment		.text
+global		_ft_bzero
+extern		_ft_memset
+
+_ft_bzero:
+	push	rbp
+	mov		rbp, rsp
+	mov		rdx, rsi			; place size = 2nd arg of ft_bzero as 3rd arg of ft_memset
+	xor		rsi, rsi			; 2nd arg of ft_memset = 0
+	call	_ft_memset
+	leave
+	ret
