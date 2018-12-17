572c620b384378fd63e1ce8e0fd8217cbc115569
Mthokozisi MOHAULE <mmohaule@e4r13p10.wethinkcode.co.za>
2018-12-17 09:04:38 +0200

    changed .asm to .s

--- src/ft_bzero.s
+++ src/ft_bzero.s
@@ -5,8 +5,8 @@ extern		_ft_memset
 _ft_bzero:
 	push	rbp
 	mov		rbp, rsp
-	mov		rdx, rsi			; 2nd arg of ft_bzero as 3rd arg of ft_memset
-	xor		rsi, rsi			; 2nd arg of ft_memset = 0
+	mov		rdx, rsi		; 2nd arg of ft_bzero as 3rd arg of ft_memset
+	xor		rsi, rsi		; 2nd arg of ft_memset = 0
 	call	_ft_memset
 	leave
 	ret
