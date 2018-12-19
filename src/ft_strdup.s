cdc6b4978348bd879c4f705ea9cb70b346464fc5
Mthokozisi MOHAULE <mmohaule@e4r13p10.wethinkcode.co.za>
2018-12-19 13:39:45 +0200

    modified ft_strdup

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -8,16 +8,16 @@ extern		_ft_memcpy
 _ft_strdup:
 	push	rbp
 	mov		rbp, rsp
-	mov		rbx, rdi				; store str ptr in callee-saved register
+	mov		rbx, rdi		; s1
 	call	_ft_strlen
-	inc		rax						; add 1 to strlen for null char
-	mov		r12, rax				; store strlen in callee-saved register
-	mov		rdi, rax				; set strlen as malloc arg
+	inc		rax				; +1 for null char
+	mov		r12, rax		; strlen
+	mov		rdi, rax		; strlen as malloc argument
 	call	_malloc
-	mov		r13, rax				; store ptr of allocated memory in callee-saved register
-	mov		rdi, rax
-	mov		rsi, rbx
-	mov		rdx, r12
+	mov		r13, rax		; ptr to allocated memory (s2)
+	mov		rdi, rax		; ft_memcpy dst	
+	mov		rsi, rbx		; ft_memcpy src
+	mov		rdx, r12		; ft_memcpy len
 	call	_ft_memcpy
 	mov		rax, r13
 	leave
